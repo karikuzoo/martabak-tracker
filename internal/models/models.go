5f1af446498a914f2aed36767e17b54fbcede086
Rivaldy Askar <aldyrafi@gmail.com>
2026-08-12 00:04:54 +0700

    add user

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,22 +9,24 @@ import (
 
 type DBModel struct {
 	Order OrderModel
+	User  UserModel
 }
 
 func InitDB(dataSourceName string) (*DBModel, error) {
 	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{})
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to migrate database: %v", err)
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
 	}
 
-	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
 	if err != nil {
-		return nil, fmt.Errorf("Failed to migrate database %v", err)
+		return nil, fmt.Errorf("failed to migrate database %v", err)
 	}
 
 	dbModel := &DBModel{
 		Order: OrderModel{DB: db},
+		User:  UserModel{DB: db},
 	}
 	return dbModel, nil
 }
